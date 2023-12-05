2a378344608fcc04ba16fbdc2447ffc39191b2eb
RokasV <vezikauskas.r@gmail.com>
2023-12-05 11:17:20 +0200

    deleteWorks

--- js/main.js
+++ js/main.js
@@ -29,6 +29,7 @@ async function getPosts() {
 
 // sugeneruoti postus HTMLe i ID postList
 function makePostsHtml(arr) {
+  els.postListEl.innerHTML = "";
   arr.forEach((postObj) => {
     const singlePostEl = createSinglePostEl(postObj);
     els.postListEl.append(singlePostEl);
@@ -39,17 +40,54 @@ function createSinglePostEl(singlePostObj) {
   const liEl = document.createElement("li");
   const innerDiv = `
   <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">${singlePostObj.title}</h5>
-              <h6 class="card-subtitle mb-2 text-body-secondary">
-              ${singlePostObj.author}
-              </h6>
-              <p class="card-text">
-              ${singlePostObj.body.slice(0, 75)}...
-              </p>
-              <a href="single-post.html" class="btn btn-primary card-link">Read more</a>
-            </div>
-          </div>`;
+    <div class="card-header">
+      id: ${singlePostObj.id}
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">${singlePostObj.title}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">
+      ${singlePostObj.author}
+      </h6>
+      <p class="card-text">
+      ${singlePostObj.body.slice(0, 75)}...
+      </p>
+      <a href="single-post.html" class="btn btn-primary card-link">Read more</a>
+    </div>
+  </div>`;
+
   liEl.innerHTML = innerDiv;
+  // sukurti mygtuka delete
+  const deleteBtnEl = document.createElement("button");
+  deleteBtnEl.textContent = "Delete";
+  deleteBtnEl.classList.add("btn", "btn-outline-danger");
+  deleteBtnEl.addEventListener("click", () =>
+    sendDeleteFetch(singlePostObj.id)
+  );
+
+  liEl.querySelector(".card-body").append(deleteBtnEl);
+
   return liEl;
+}
+
+function sendDeleteFetch(idToDelete) {
+  console.log("deleting post", idToDelete);
+  //fetch su metodu delete
+  fetch(`${postsUrl}/${idToDelete}`, {
+    method: "DELETE",
+  })
+    .then((resp) => {
+      if (resp.status === 200) {
+        console.log("istrinti pavyko");
+        init();
+      } else {
+        console.log("istrinti nepavyko");
+      }
+      return resp.json();
+    })
+    .then((rez) => {
+      console.log(rez);
+    })
+    .catch((error) => {
+      console.warn("ivyko klaida trinant", error);
+    });
 }
