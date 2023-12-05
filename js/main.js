545517e7b305c4fd8859051e99249bf3957bf032
RokasV <vezikauskas.r@gmail.com>
2023-12-05 09:07:16 +0200

    single post html, js start

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 "use strict";
 console.log("main.js file was loaded");
+
+const baseUrl = "http://localhost:5000";
+const postsuRL = `${baseUrl}/posts`;
+
+// sukurti funkcija kuri parsiuncia ir iskonsolina visus postus
+
+// sugeneruoti postus HTMLe i ID postList
