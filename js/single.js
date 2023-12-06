f988805b7d9dfb24f530d597ff47ead45178bc06
RokasV <vezikauskas.r@gmail.com>
2023-12-06 09:47:52 +0200

    single post veikia

--- js/single.js
+++ js/single.js
@@ -3,7 +3,15 @@ console.log("single.js file was loaded");
 const baseUrl = "http://localhost:5000";
 let currentPostId = "8374820193847912";
 const postsUrl = `${baseUrl}/posts`;
-const els = {};
+const els = {
+  title: document.getElementById("title"),
+  image: document.getElementById("image"),
+  body: document.getElementById("body"),
+  author: document.getElementById("author"),
+  date: document.getElementById("date"),
+  tags: document.getElementById("tags"),
+};
+console.log("els ===", els);
 
 // gauti id is url
 const urlParamsObj = new URLSearchParams(window.location.search);
@@ -25,9 +33,9 @@ function getSinglePost(address) {
       }
       return resp.json();
     })
-    .then((rez) => {
-      console.log(rez);
-      fillHtmlPage(rez);
+    .then((obj) => {
+      console.log(obj);
+      fillHtmlPage(obj);
     })
     .catch((error) => {
       console.warn("negavom duomenu", error);
@@ -35,4 +43,18 @@ function getSinglePost(address) {
 }
 getSinglePost(`${postsUrl}/${currentPostId}`);
 
-function fillHtmlPage() {}
+function fillHtmlPage(rez) {
+  els.title.textContent = rez.title;
+  els.image.src = rez.image;
+  els.body.textContent = rez.body;
+  els.author.textContent = rez.author;
+  els.date.textContent = rez.date;
+  els.tags.innerHTML = "";
+  console.log("els.tags ===", els.tags);
+  rez.tags.forEach((obj) => {
+    const liEl = document.createElement("li");
+    liEl.classList.add("badge", "rounded-pill", "text-bg-success", "fs-5");
+    liEl.textContent = obj;
+    els.tags.append(liEl);
+  });
+}
